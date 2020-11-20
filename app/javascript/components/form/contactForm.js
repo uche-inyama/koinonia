f762d295e1841b289a6b762101b1e8857a1eee68
uche-inyama <depoulet@yahoo.com>
2020-11-20 11:30:02 +0100

    add validations to contact form

--- app/javascript/components/form/contactForm.js
+++ app/javascript/components/form/contactForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { useForm } from 'react-hook-form'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 
 const Contactform = styled.div`
   padding: 20px;
-  border: 1px solid red;
+  border: 1px solid grey;
   border-radius: 5px;
   font-family: 'Open Sans', sans-serif;
   text-transform: capitalize;
@@ -26,6 +27,7 @@ const Contactform = styled.div`
     display: flex;
     flex-direction: column;
     height: 70px;
+    font-size: 0.8em;
     .submit{
       margin-top: 20px;
       border-radius: 3px;
@@ -33,7 +35,10 @@ const Contactform = styled.div`
     input{
       height: 35px;
       border-radius: 5px;
-      border: 1px solid red;
+      border: 1px solid grey;
+    }
+    p {
+      color: red;
     }
     label{
       margin-bottom: 2px;
@@ -42,7 +47,7 @@ const Contactform = styled.div`
       min-height: 100px;
       max-height: 200px;
       border-radius: 5px;
-      border: 1px solid red;
+      border: 1px solid gray;
     }
   }
   .space{
@@ -56,8 +61,9 @@ const Contactform = styled.div`
 const contactForm = () => {
 
   const [contact, setContact] = useState({})
+  const { register, errors, handleSubmit } = useForm();
 
-  const handleSubmit = (e) => {
+  const onSubmit = (e) => {
     axios.post('/api/v1/contacts', { contact })
       .catch(err => err);
   }
@@ -66,38 +72,34 @@ const contactForm = () => {
     setContact(Object.assign({}, contact, { [e.target.name]: e.target.value }));
   }
 
-
-  // const handleFormSubmit = (e) => {
-  //   e.preventDefault()
-  //   onSubmitClick(newPartner)
-  //   setNewPartner({})
-  // }
-
-
   return (
     <Contactform>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <ul className="form-items-wrapper">
           <li className="name-mobile">
             <div className="field">
               <label>name</label>
-              <input type="text" name="name" onChange={handleChange} />
+              <input className="input-field" ref={register({ required: true, maxLength: 20 })} type="text" name="name" onChange={handleChange} />
+              {errors.name && <p>name is required</p>}
             </div>
             <div className="field space">
               <label>mobile number</label>
-              <input type="text" name="subject" onChange={handleChange} />
+              <input className="input-field" ref={register({ required: true, minLength: 6, maxLength: 12 })} type="text" name="subject" onChange={handleChange} />
+              {errors.subject && <p>mobile number is required</p>}
             </div>
           </li>
           <li>
             <div className="field">
               <label>email</label>
-              <input type="text" name="email" onChange={handleChange} />
+              <input className="input-field" ref={register({ required: true, pattern: /^\S+@\S+$/i })} type="text" name="email" onChange={handleChange} />
+              {errors.email && <p>email is required</p>}
             </div>
           </li>
           <li>
             <div className="field">
               <label>message</label>
-              <textarea type="text" name="message" rows="50" cols="30" onChange={handleChange} />
+              <textarea className="input-field" ref={register({ required: true })} type="text" name="message" rows="50" cols="30" onChange={handleChange} />
+              {errors.message && <p>message is required</p>}
             </div>
           </li>
           <li>
